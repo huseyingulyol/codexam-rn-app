bc73c9d82fcf6e0c01218d68079c5ecbe6bc96e8
eminunal <mhmtemn001@gmail.com>
2025-02-25 13:24:05 +0300

    add RegisterScreen

---
+++ components/BackButton.tsx
@@ -0,0 +1,33 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { StyleSheet } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
+export default function BackButton({ navigation } : any) {
+  return (
+    <View style={styles.buttonBack}>
+      <TouchableOpacity style={styles.buttonBack} onPress={() => navigation.goBack()}>
+        <View> 
+            <Ionicons name="arrow-back" size={40} color="black" />
+        </View>
+      </TouchableOpacity>
+    </View>
+    
+  )
+}
+
+const styles = StyleSheet.create({
+  
+buttonBack:{
+    width: 120,
+    height: 50,
+    backgroundColor: "#cdd6f4",
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    
+  }
+  
+});
+
---
+++ env.d.ts
@@ -0,0 +1,4 @@
+declare module '@env' {
+    export const PB_EMAIL: string;
+    export const PB_PASSWORD: string;
+  }
